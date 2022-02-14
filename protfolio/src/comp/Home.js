21cb66d860df627010e472e5f898a8b78e5f4fb7
almujahedmarah <marah.almujahed.ruh.js@tuwaiq.edu.sa>
2022-02-14 22:27:58 +0300

    done

--- protfolio/src/comp/Home.js
+++ protfolio/src/comp/Home.js
@@ -20,7 +20,7 @@ export default function Home() {
         </div>
       </div>
         <About />
-        <Vid/>
+        {/* <Vid/> */}
     </div>
   );
 }
--- protfolio/src/comp/Vid.js
+++ protfolio/src/comp/Vid.js
@@ -1,51 +1,51 @@
-import React from "react";
+// import React from "react";
 
-export default function Vid() {
-  return (
-    <div className="videohh" id="r">
-      <h2  className="titlepro">Dior store </h2>
-      <div className="vide">
-        <div className="vidp">
-        <p className="r"> frontend</p>
-        <p className="rrr"> Reactjs, Axios,  CSS, react-spring</p>
-        <p className="r"> backend</p>
-        <p className="rrr"> Express, Nodejs , Mongodb (Atlas),JWT,postman</p>
-        <a className="link1" href="https://dior-store.herokuapp.com/" >Click me to see the Project</a>
-        </div>
-   <iframe width="1000px" height="400px"  src="https://rr2---sn-hgn7yn7s.c.drive.google.com/videoplayback?expire=1644867973&ei=RXkKYtDDCObz1bYP4oi5gA4&ip=2001:16a2:cb90:9f00:b536:c631:6133:d3e6&cp=QVRJVklfVllVRFhPOmRzMEoxRkI2MzNDalZRcm11RmxNdXM1aHlhaWtNTEFCelhzWGhpb2wzcEE&id=bfab4aa065d008c9&itag=18&source=webdrive&requiressl=yes&mh=y2&mm=32&mn=sn-hgn7yn7s&ms=su&mv=m&mvi=2&pl=40&ttl=transient&susc=dr&driveid=10lE2XIbet96ah60yGiznBe_auTuUzHMO&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=151.765&lmt=1644742953651555&mt=1644853225&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRQIhAMG1XlW7bnIMWD06Es92fK3yPMZnI9Qlg-ZghU6iLJIQAiAInweBxBpFPefo6RiMRMgNAicfnBqMc5oI8fu2KsBq7A==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgbn4x5ey1bheOi2d6XQDCNABVTp1TrCSMJv7EK-lF_4ECIQCXQWqMytPez1KBKY2akC43M7h-3VFiiYsHMkLTDCM0Vw==&cpn=BfUjhJrHHl84QMpg&c=WEB_EMBEDDED_PLAYER&cver=1.20220208.01.00"></iframe>
-      </div>
-      <h2  className="titlepro">Flower shop </h2>
-      <div className="vide">
-      <iframe  width="1000px" height="400px" src="https://rr4---sn-hgn7rnee.c.drive.google.com/videoplayback?expire=1644868931&ei=A30KYv6_Bu_3wtQP8fme2A4&ip=2001:16a2:cb90:9f00:b536:c631:6133:d3e6&cp=QVRJVklfV1lRQlhPOmRzMEsxQlo2MzNDalZScmlzRmxNdXM1aXl3Z2tNTEFCellzVGZpb2wzcEE&id=abebca3af58fac39&itag=18&source=webdrive&requiressl=yes&mh=cY&mm=32&mn=sn-hgn7rnee&ms=su&mv=m&mvi=4&pl=40&sc=yes&ttl=transient&susc=dr&driveid=1Bt9HyHzFXmdI0OtS0Z4FmeMzYE7Qk7um&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=51.617&lmt=1644744746323201&mt=1644854186&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRQIgF-0jaIMO9sFZDFKg9R7z4KzECJSFXCHymfF07977uFACIQCfRbV3Kyv2MPwv81MKjgCAfQTpBrKxUF6F92xPhg9ExQ==&lsparams=mh,mm,mn,ms,mv,mvi,pl,sc&lsig=AG3C_xAwRAIgJPP-Pvb-QZEf85v5-agfDHGC6a0W2ZrM9xXKCoVAKq0CID-4bVW4pyTPhebypciaFA-7vvp1wgIVYAg4_tlvLTHU&cpn=QFy7WKiPyK9n5lo0&c=WEB_EMBEDDED_PLAYER&cver=1.20220208.01.00"></iframe>
-      <div className="vidp">
-        <p className="r"> frontend</p>
-        <p className="rrr"> Reactjs, Axios, Bootstrap, Redux</p>
-        <p className="r"> backend</p>
-        <p className="rrr"> Express, Nodejs , postman</p>
-        </div>
-      </div>
-      <h2  className="titlepro">Doctor app</h2>
-      <div className="vide">
-      <div className="vidp">
-        <p className="r"> frontend</p>
-        <p className="rrr"> Reactjs, Axios,  CSS, Bootstrap</p>
-        <p className="r"> backend</p>
-        <p className="rrr">Express, Nodejs , Mongodb (Atlas),JWT,postman</p>
-        </div>
+// export default function Vid() {
+//   return (
+//     <div className="videohh" id="r">
+//       <h2  className="titlepro">Dior store </h2>
+//       <div className="vide">
+//         <div className="vidp">
+//         <p className="r"> frontend</p>
+//         <p className="rrr"> Reactjs, Axios,  CSS, react-spring</p>
+//         <p className="r"> backend</p>
+//         <p className="rrr"> Express, Nodejs , Mongodb (Atlas),JWT,postman</p>
+//         <a className="link1" href="https://dior-store.herokuapp.com/" >Click me to see the Project</a>
+//         </div>
+//    <iframe width="1000px" height="400px"  src="https://rr2---sn-hgn7yn7s.c.drive.google.com/videoplayback?expire=1644867973&ei=RXkKYtDDCObz1bYP4oi5gA4&ip=2001:16a2:cb90:9f00:b536:c631:6133:d3e6&cp=QVRJVklfVllVRFhPOmRzMEoxRkI2MzNDalZRcm11RmxNdXM1aHlhaWtNTEFCelhzWGhpb2wzcEE&id=bfab4aa065d008c9&itag=18&source=webdrive&requiressl=yes&mh=y2&mm=32&mn=sn-hgn7yn7s&ms=su&mv=m&mvi=2&pl=40&ttl=transient&susc=dr&driveid=10lE2XIbet96ah60yGiznBe_auTuUzHMO&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=151.765&lmt=1644742953651555&mt=1644853225&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRQIhAMG1XlW7bnIMWD06Es92fK3yPMZnI9Qlg-ZghU6iLJIQAiAInweBxBpFPefo6RiMRMgNAicfnBqMc5oI8fu2KsBq7A==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgbn4x5ey1bheOi2d6XQDCNABVTp1TrCSMJv7EK-lF_4ECIQCXQWqMytPez1KBKY2akC43M7h-3VFiiYsHMkLTDCM0Vw==&cpn=BfUjhJrHHl84QMpg&c=WEB_EMBEDDED_PLAYER&cver=1.20220208.01.00"></iframe>
+//       </div>
+//       <h2  className="titlepro">Flower shop </h2>
+//       <div className="vide">
+//       <iframe  width="1000px" height="400px" src="https://rr4---sn-hgn7rnee.c.drive.google.com/videoplayback?expire=1644879919&ei=76cKYua1FdCB1dUP_NOnmAw&ip=2001:16a2:cb90:9f00:b536:c631:6133:d3e6&cp=QVRJVklfWFlPSlhPOkJXSmQ1ME9PakdubTdoVEwtZ2RiZTBUa2xJYzlZZENSSzg0Szl4S1RGRjU&id=abebca3af58fac39&itag=18&source=webdrive&requiressl=yes&mh=cY&mm=32&mn=sn-hgn7rnee&ms=su&mv=m&mvi=4&pl=40&ttl=transient&susc=dr&driveid=1Bt9HyHzFXmdI0OtS0Z4FmeMzYE7Qk7um&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=51.617&lmt=1644744746323201&mt=1644865223&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRQIgE_xaJ_mSYv4bMPAA7HtzAVpnh-VIgqJdqtJ-VEtNWCoCIQChEsdmdb2aUWxEq_CHnJHOG3o1RxsKjUAhj0qWSHYFfQ==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhAJ2jxM2VhfJ1mSc1tW7YHCPmamI1Lz4-GM1Wack4NXQ8AiEAlJzPFlIekIxOztuLHEwFKhXtEi7FADNrnyz1kTeEeHM=&cpn=Okjw_aAeHt0xg8nn&c=WEB_EMBEDDED_PLAYER&cver=1.20220208.01.00"></iframe>
+//       <div className="vidp">
+//         <p className="r"> frontend</p>
+//         <p className="rrr"> Reactjs, Axios, Bootstrap, Redux</p>
+//         <p className="r"> backend</p>
+//         <p className="rrr"> Express, Nodejs , postman</p>
+//         </div>
+//       </div>
+//       <h2  className="titlepro">Doctor app</h2>
+//       <div className="vide">
+//       <div className="vidp">
+//         <p className="r"> frontend</p>
+//         <p className="rrr"> Reactjs, Axios,  CSS, Bootstrap</p>
+//         <p className="r"> backend</p>
+//         <p className="rrr">Express, Nodejs , Mongodb (Atlas),JWT,postman</p>
+//         </div>
         
-        <video className="video" controls>
-          <source src="https://i.imgur.com/nmGm0vv.mp4" type="video/mp4" />
-        </video>
-      </div>
-      <h2 className="titlepro">pokodex</h2>
-      <div className="vide">
-      <iframe width="1000px" height="400px"  src="https://rr5---sn-hgn7yn7z.c.drive.google.com/videoplayback?expire=1644869241&ei=OX4KYu_lLYmspb0PhbyoyAQ&ip=2001:16a2:cb90:9f00:b536:c631:6133:d3e6&cp=QVRJVklfWFJSQlhPOmRzMEw0Q1o2MzNDalZTa2pzRmxNdXM1anJ4Z2tNTEFCelpsVWZpb2wzcEE&id=787f0c90f9826123&itag=18&source=webdrive&requiressl=yes&mh=fj&mm=32&mn=sn-hgn7yn7z&ms=su&mv=m&mvi=5&pl=40&ttl=transient&susc=dr&driveid=13SSEtYdLse6aE06YlX6WPvcp0maqfn9A&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=11.726&lmt=1644350917386512&mt=1644854425&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRAIgTSIEVyqQ80OIJdoHliqpBTLH-qTZSf60pC11Y8GFxSsCIHttMbP-puDuxPiJAM9gUnxt5t6MSIyYzh215uyaAYkr&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgf2XlI6vLQCcArus-P_cqHzuDiX8p1hs-XRSyBVj-NMQCIQCBuZwyndA0SizoRsQKsH4TkpeiwhZWNEpDLa94oefNSw==&cpn=dqT46sWN2E4G4Cjq&c=WEB_EMBEDDED_PLAYER&cver=1.20220208.01.00"></iframe>
-      <div className="vidp">
-        <p className="r"> frontend</p>
-        <p className="rrr"> Reactjs, Axios,  CSS</p>
-        </div>
-      </div>
+//         <video className="video" controls>
+//           <source src="https://i.imgur.com/nmGm0vv.mp4" type="video/mp4" />
+//         </video>
+//       </div>
+//       <h2 className="titlepro">pokodex</h2>
+//       <div className="vide">
+//       <iframe width="1000px" height="400px"  src="https://rr5---sn-hgn7yn7z.c.drive.google.com/videoplayback?expire=1644869241&ei=OX4KYu_lLYmspb0PhbyoyAQ&ip=2001:16a2:cb90:9f00:b536:c631:6133:d3e6&cp=QVRJVklfWFJSQlhPOmRzMEw0Q1o2MzNDalZTa2pzRmxNdXM1anJ4Z2tNTEFCelpsVWZpb2wzcEE&id=787f0c90f9826123&itag=18&source=webdrive&requiressl=yes&mh=fj&mm=32&mn=sn-hgn7yn7z&ms=su&mv=m&mvi=5&pl=40&ttl=transient&susc=dr&driveid=13SSEtYdLse6aE06YlX6WPvcp0maqfn9A&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=11.726&lmt=1644350917386512&mt=1644854425&txp=0011224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRAIgTSIEVyqQ80OIJdoHliqpBTLH-qTZSf60pC11Y8GFxSsCIHttMbP-puDuxPiJAM9gUnxt5t6MSIyYzh215uyaAYkr&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgf2XlI6vLQCcArus-P_cqHzuDiX8p1hs-XRSyBVj-NMQCIQCBuZwyndA0SizoRsQKsH4TkpeiwhZWNEpDLa94oefNSw==&cpn=dqT46sWN2E4G4Cjq&c=WEB_EMBEDDED_PLAYER&cver=1.20220208.01.00"></iframe>
+//       <div className="vidp">
+//         <p className="r"> frontend</p>
+//         <p className="rrr"> Reactjs, Axios,  CSS</p>
+//         </div>
+//       </div>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
